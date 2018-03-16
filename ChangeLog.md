cc3dd634e46de0920b85d6e68310535071489dd5
Laurent Marquet <laurent.marquet@laposte.net>
2018-03-16 14:36:40 +0100

    v1.0 ---- - Creation of bundle (16/03/2018)

---
+++ ChangeLog.md
@@ -0,0 +1,5 @@
+# Changelog
+
+v1.0
+----
+- Creation of bundle (16/03/2018)
---
+++ c975LPurchaseCreditsBundle.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * (c) 2018: 975L <contact@975l.com>
+ * (c) 2018: Laurent Marquet <laurent.marquet@laposte.net>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace c975L\PurchaseCreditsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class c975LPurchaseCreditsBundle extends Bundle
+{
+}
